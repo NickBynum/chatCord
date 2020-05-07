5de8da5aa8652bacab3a8b5b1cb6109efc1a0084
Nick Bynum <exander@live.com>
2020-05-07 14:30:33 -0600

    stopping point save

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const http = require('http');
 const app = express();
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+  console.log('New WS Connection...');
+
+  socket.emit('msg', 'Welcome to chatCord')
+});
 
 const PORT = process.env.PORT || 3000;
 
